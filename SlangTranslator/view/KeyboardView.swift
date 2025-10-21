e71cdc6b05c6e8320250f9d0b1dc2cf74cacab1c
canquinee <cynthiayapiter760@gmail.com>
2025-10-21 14:57:34 +0700

    up view translate

---
+++ SlangTranslator/view/KeyboardView.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardView.swift
+//  IndoSlangTrans
+//
+//  Created by Cynthia Yapiter on 21/10/25.
+//
+import SwiftUI
+
+struct KeyboardView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    KeyboardView()
+}
