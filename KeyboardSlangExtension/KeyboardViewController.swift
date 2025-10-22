e65a65f3008b2a99fd7731307d01f36e326ed893
filzarr <filzaramadhan2003@gmail.com>
2025-10-22 21:23:14 +0700

    feat: structure code and keyboard

--- KeyboardSlangExtension/KeyboardViewController.swift
+++ KeyboardSlangExtension/KeyboardViewController.swift
@@ -6,55 +6,52 @@
 //
 
 import UIKit
+import SwiftUI
 
 class KeyboardViewController: UIInputViewController {
-
-    @IBOutlet var nextKeyboardButton: UIButton!
-    
-    override func updateViewConstraints() {
-        super.updateViewConstraints()
-        
-        // Add custom view sizing constraints here
-    }
+    private var hostingController: UIHostingController<SlangKeyboardView>?
+     
+    private var keyboardHeight: CGFloat = 270
     
     override func viewDidLoad() {
         super.viewDidLoad()
+         
+        let root = SlangKeyboardView(
+            insertText: { [weak self] text in
+                guard let self else { return }
+                self.textDocumentProxy.insertText(text)
+            },
+            deleteText: { [weak self] in
+                guard let self, self.textDocumentProxy.hasText else { return }
+                self.textDocumentProxy.deleteBackward()
+            },
+            keyboardHeight: keyboardHeight,
+            backgroundColor: .clear, needsInputModeSwitchKey: self.needsInputModeSwitchKey,
+            nextKeyboardAction: #selector(self.handleInputModeList(from:with:))
+        )
         
-        // Perform custom UI setup here
-        self.nextKeyboardButton = UIButton(type: .system)
+        let hosting = UIHostingController(rootView: root)
+        hosting.view.backgroundColor = .clear
+         
+        addChild(hosting)
+        view.addSubview(hosting.view)
+        hosting.didMove(toParent: self)
+         
+        hosting.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hosting.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hosting.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            hosting.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hosting.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
         
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
-        self.nextKeyboardButton.sizeToFit()
-        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
-        
-        self.view.addSubview(self.nextKeyboardButton)
-        
-        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        self.hostingController = hosting
+         
+        let heightConstraint = view.heightAnchor.constraint(equalToConstant: keyboardHeight)
+        heightConstraint.priority = .defaultHigh
+        heightConstraint.isActive = true
     }
-    
     override func viewWillLayoutSubviews() {
-        self.nextKeyboardButton.isHidden = !self.needsInputModeSwitchKey
         super.viewWillLayoutSubviews()
     }
-    
-    override func textWillChange(_ textInput: UITextInput?) {
-        // The app is about to change the document's contents. Perform any preparation here.
-    }
-    
-    override func textDidChange(_ textInput: UITextInput?) {
-        // The app has just changed the document's contents, the document context has been updated.
-        
-        var textColor: UIColor
-        let proxy = self.textDocumentProxy
-        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
-            textColor = UIColor.white
-        } else {
-            textColor = UIColor.black
-        }
-        self.nextKeyboardButton.setTitleColor(textColor, for: [])
-    }
-
 }
