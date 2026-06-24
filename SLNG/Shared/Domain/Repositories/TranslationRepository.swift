4a3d9300f284262a269d3daec135ea9752b01e8d
Ikhwan Pramuditha <ikhwanpramuditha05@gmail.com>
2026-06-24 21:09:40 +0700

    feat: setup debug and release env

---
+++ SLNG/Shared/Domain/Repositories/TranslationRepository.swift
@@ -0,0 +1,13 @@
+//
+//  TranslationRepository.swift
+//  SlangTranslator
+//
+//  Created by Pramuditha Muhammad Ikhwan on 24/10/25.
+//
+
+import Foundation
+
+protocol TranslationRepository {
+    func translateSentence(_ text: String) async throws -> TranslationResponse
+    func fetchCachedTranslation(for text: String) -> TranslationResponse?
+}
